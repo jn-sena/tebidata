4d1b3814eba9b5098f53e4a73508f829f9f56e7d
Sena <contact@sena.pink>
2025-11-10 02:58:03 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "AAL TEBİGEP Ekip Üyeleri Veritabanı";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system:
+          function {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShellNoCC {
+            name = "tebidata";
+            meta.description = "AAL TEBİGEP Ekip Üyeleri Veritabanı";
+
+            packages = builtins.attrValues {
+              inherit (pkgs)
+                just
+                deno
+                lessc
+                http-server
+                ;
+            };
+          };
+        }
+      );
+    };
+}
